74e0a239bd1c4c0caf01978940144ef29e6392eb
Whitney Lampkin <18070772+WhitneyLampkin@users.noreply.github.com>
2022-10-24 09:31:42 -0500

    adding python assignments for lecture 1 and 2

---
+++ assignments/hello-user.py
@@ -0,0 +1,5 @@
+# Send welcome msg with user's input
+
+user_name = input()
+output_sentence = 'Hi ' + user_name + ', welcome to Python!'
+print(output_sentence)
--- assignments/hello-world.py
+++ assignments/hello-world.py
@@ -1,8 +1,16 @@
+# Adding Numbers
+# Write a program that reads from the user two number, and prints their sum.
+
 print('Please enter two numbers on separate lines:')
 num1 = int(input())
 num2 = int(input())
 sum = num1 + num2
 
-output_sentence = str(num1) + ' + ' + str(num2) + ' = ' + str(sum)
+print(num1, '+', num2, '=', sum)
 
+"""
+Alternative Output
+
+output_sentence = str(num1) + ' + ' + str(num2) + ' = ' + str(sum)
 print(output_sentence)
+"""
