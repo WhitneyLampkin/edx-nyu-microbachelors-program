bfae10c85f6c31ecb19e2bcd40dbe71e6cba43c9
Whitney Lampkin <18070772+WhitneyLampkin@users.noreply.github.com>
2022-11-28 16:25:52 -0600

    adding bmi metric lab instructions

---
+++ labs/bmimetric.py
@@ -0,0 +1,30 @@
+"""
+Description
+
+Body mass index (BMI) is a number calculated from  a person’s weight and height.
+According to the Centers for Disease Control and Prevention, the BMI is a fairly
+reliable indicator of body fatness for most people. BMI does not measure body fat
+directly, but research has shown that  BMI correlates to direct  measures  of  body  fat,  
+such  as  underwater  weighing  and dual-energy X-ray absorptiometry. The formula 
+for BMI is
+    
+Weight/Height2
+
+Where weight is in kilograms and height is in meters.
+
+
+Write a program that prompts for metric weight and height and outputs the BMI.
+
+
+For example, an execution could look  like  this:
+Please enter weight in kilograms: 50
+Please enter height in meters: 1.58
+BMI is: 20.0288415318
+
+
+File Name 
+bmimetric.py
+
+Score
+There are five tests each worth 2 points
+"""
