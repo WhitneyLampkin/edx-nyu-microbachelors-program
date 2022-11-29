1bc72f8418c4fed4385bd190e4c15a02b757854e
Whitney Lampkin <18070772+WhitneyLampkin@users.noreply.github.com>
2022-11-29 09:32:01 -0600

    adding file and instructions for bmi imperial lab

---
+++ labs/bmiimperial.py
@@ -0,0 +1,30 @@
+"""
+Description
+Body  mass  index (BMI) is  a number  calculated  from  a person’s weight and height.
+According to the Centers for Disease Control and Prevention, the BMI is a fairly
+reliable indicator of body fatness for most people. BMI does not measure body fat
+directly, but research  has shown that  BMI correlates  to  direct  measures  of  body  fat,  
+such  as  underwater  weighing  and dual-energy X-ray absorptiometry. The formula 
+for BMI is
+    Weight/Height2
+
+Where    weight is in kilograms    and height is in meters.
+
+Write a program that prompts for weight in pounds and height in inches, 
+converts  the values to metric, and then calculates the BMI.
+Note: 1 pound is  0.453592  kilograms and 1 inch is 0.0254  meters.
+
+
+For example, an execution could look like this:
+Please enter weight in pounds: 135
+Please enter height in inches: 71
+BMI is: 18.8284697141
+
+
+File Name 
+bmiimperial.py
+
+
+Score
+There are five tests each worth 2 points
+"""
