c513b2d3e411d5ff091863b03519757159697500
Whitney Lampkin <18070772+WhitneyLampkin@users.noreply.github.com>
2022-12-08 18:52:31 -0600

    adding absolute value assignment

---
+++ assignments/4-computing-absolute-value.py
@@ -0,0 +1,10 @@
+## Get user input
+print("Please enter an integer:")
+user_input = int(input())
+
+## Calculate Abs Value
+if(user_input < 0):
+    user_input = user_input * (-1)
+
+## Display Abs Value
+print(user_input)
