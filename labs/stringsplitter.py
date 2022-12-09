6f77ee6907a57741d55c2a26babc14f2ad1aee91
Whitney Lampkin <18070772+WhitneyLampkin@users.noreply.github.com>
2022-12-09 17:45:13 -0600

    add stringsplitter lab

---
+++ labs/stringsplitter.py
@@ -0,0 +1,26 @@
+"""
+Description
+
+Read from the user a string containing 
+odd number of characters. Your program should: 
+a) Print the middle character. 
+b) Print the string up to but not including the middle character (i.e., the first half 
+of the string). 
+c) Print the string from the middle character to the end (not including the 
+middle character). 
+
+Sample output: 
+Enter an odd length string: Fortune favors the bold
+Middle character: o
+First half: Fortune fav
+Second half: rs the bold
+
+File Name 
+
+stringsplitter.py
+
+Score
+
+There are five tests each worth 2 points
+"""
+
