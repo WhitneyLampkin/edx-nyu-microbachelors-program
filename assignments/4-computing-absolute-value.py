d6c6166384d27076bf239909b7b6594da871f247
Whitney Lampkin <18070772+WhitneyLampkin@users.noreply.github.com>
2022-12-08 19:00:22 -0600

    final code for computing abs value assignment

--- assignments/4-computing-absolute-value.py
+++ assignments/4-computing-absolute-value.py
@@ -2,9 +2,12 @@
 print("Please enter an integer:")
 user_input = int(input())
 
+abs_value = user_input
+
 ## Calculate Abs Value
 if(user_input < 0):
-    user_input = user_input * (-1)
+    abs_value = user_input * (-1)
 
 ## Display Abs Value
-print(user_input)
+## Note: sep="" overrides the default space separator for print()
+print("|", user_input, "| = ", abs_value, sep="")
