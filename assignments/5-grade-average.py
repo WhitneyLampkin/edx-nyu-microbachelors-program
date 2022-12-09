f1472b46c9150b2ae2d9c5452a0e165c62f7b5fa
Whitney Lampkin <18070772+WhitneyLampkin@users.noreply.github.com>
2022-12-09 16:51:57 -0600

    adding grade averager

---
+++ assignments/5-grade-average.py
@@ -0,0 +1,13 @@
+print("Please enter the number of students in the class:")
+number_of_students = int(input())
+
+print("Please enter the students' grades (in separate lines):")
+
+grades_sum = 0
+
+for i in range(number_of_students):
+    curr_grade = int(input())
+    grades_sum += curr_grade
+
+average = grades_sum / number_of_students
+print("The class average is", average)
